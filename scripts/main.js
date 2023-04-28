7ba7b27a4ee4b8a5d71129a4e79c3c2c36b7024d
kevinelong <kevinelong@gmail.com>
2023-04-28 10:35:14 -0700

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
+    const email = document.getElementById("email");
+    const registerButton = document.getElementById("register_button");
+    const outputMessage = document.getElementById("output_message");
+
+    registerButton.addEventListener("click", ()=>{
+        outputMessage.innerHTML = `
+            Welcome ${email.value}!!!
+        `;
+    });//END BUTTON CLICK
+}); //END DOM CONTENT LOADED
